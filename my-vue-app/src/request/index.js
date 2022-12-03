6010cd0948a1c515280083b930e3dd7eb6531544
dqzhang <2435563017@qq.com>
2022-12-03 09:40:28 +0000

    删除文件 my-vue-app

--- my-vue-app/src/request/index.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-let service=axios.create({
-    baseURL: "http://localhost:3000/",
-    tlmeout:3000
-})
-
-export default service
--- my-vue-app/src/views/home/NowPlaying.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <!-- <p>{{$store.state.name}}</p> -->
-    </div>
-</template>
